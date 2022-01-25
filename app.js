429862acaad8c322dcef07abc59c8a0c70c85acb
loneWolf <chotansharma@yahoo.com>
2022-01-25 14:15:48 -0500

    update the file' clear
    git clear

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const [name, github] = profileDataArgs;
 
 
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
+fs.writeFile(`index.html`, generatePage(name, github), err => {
     if (err) throw new Error(err);
 
     console.log('Portfolio complete! Check out index.html to see the output!');
