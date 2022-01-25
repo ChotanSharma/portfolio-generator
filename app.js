bdaa3e5c59cee0e83ec627f12d684d3322ba7810
loneWolf <chotansharma@yahoo.com>
2022-01-25 11:53:59 -0500

    update the file

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const inquirer = require('inquirer');
 
-const printProfileData = (profileDataArr) => {
-    /*for(let i = 0; i < profileDataArr.length; i++) {
-        console.log(profileDataArr[i]);
-    }*/
-    profileDataArr.forEach(profileData => console.log(profileData));
-  };
-  
-printProfileData(profileDataArgs);
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
+
+// const pageHTML = generatePage(name, github);
+
+// fs.writeFile('./index.html', pageHTML, err => {
+  // if (err) throw err;
+
+  // console.log('Portfolio complete! Check out index.html to see the output!');
+// });
+
+const promptUser = () => {
+  return inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+}
+.then(answers => console.log(answers));
+
